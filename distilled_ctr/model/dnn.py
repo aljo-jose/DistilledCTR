59a59b4f05dfcbbc89201416848226079b77622d
aljo <aljo@novasolution.net>
2021-02-27 22:03:41 +0400

    dnn 128

--- distilled_ctr/model/dnn.py
+++ distilled_ctr/model/dnn.py
@@ -10,9 +10,9 @@ class DNNModel(nn.Module):
         super(DNNModel,self).__init__()
         self.embedding = FeaturesEmbedding(field_dims, embed_dim)
         self.embed_output_dim = len(field_dims) * embed_dim
-        self.lin1 = nn.Linear(self.embed_output_dim, 32)
-        self.lin2 = nn.Linear(32,32)
-        self.lin3 = nn.Linear(32,1)
+        self.lin1 = nn.Linear(self.embed_output_dim, 128)
+        self.lin2 = nn.Linear(128,128)
+        self.lin3 = nn.Linear(128,1)
         #self.drop_out_1 = torch.nn.Dropout(p=dropout)
         
     def forward(self, x, sigmoid_output=True):
